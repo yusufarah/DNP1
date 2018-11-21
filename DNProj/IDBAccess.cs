9ea93c3f982bb2e0f3ff816dc595f3b21ae73bf7
spaceCampBoy <260101@via.dk>
2018-11-21 13:52:48 +0100

    IDBAccess

---
+++ DNProj/IDBAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VIACinemaDB.Model;
+
+namespace DNProj
+{
+    interface IDBAccess
+    {
+        IEnumerable<Movie> getAllMovies();
+        bool makeNewReservation(int seatNumber, int scheduleID, string email);
+
+    }
+}
